4fd9fc1a21a6bf688d84d79145fa86e371306794
Deepansh Pandey <easypandey.dp@gmail.com>
2023-10-29 22:37:10 +0100

    Add  State and Component

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,25 +1,37 @@
+import {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://deepanshpandey.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Visit My Website
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      name: 'Deepansh Pandey',
+      company: 'Start Your Masters',
+    }
+  }
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p> Hi {this.state.name}! </p>
+          <button 
+            onClick={() => {
+              this.setState({name:'TDP'});
+          }}> Change Name </button>
+          <a
+            className="App-link"
+            href="https://deepanshpandey.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Visit My Website
+          </a>
+        </header>
+      </div>
+    );
+  }  
 }
 
 export default App;
